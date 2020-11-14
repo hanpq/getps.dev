1df3598cfa56ead70ebc2d5df90906c787730f7d
Hannes Palmquist <33247576+hanpq@users.noreply.github.com>
2020-11-14 22:44:11 +0100

    Added about me

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,9 +21,13 @@ module.exports = {
                     label: 'Docs',
                     position: 'left',
                 },
-                { to: 'blog', label: 'Blog', position: 'left' },
                 {
-                    href: 'https://github.com/facebook/docusaurus',
+                    to: 'blog',
+                    label: 'Blog',
+                    position: 'left'
+                },
+                {
+                    href: 'https://github.com/hanpq/getps.dev',
                     label: 'GitHub',
                     position: 'right',
                 },
---
+++ src/pages/About.md
@@ -0,0 +1,13 @@
+﻿---
+title: my hello page title
+description: my hello page description
+hide_table_of_contents: true
+---
+
+# Hannes Palmquist
+
+Hannes work as a senior consultant at Crayon Sweden primarily within Messaging, Automation, Infrastructure, Developing and Identity Management. Hannes has great experience in migration of workloads to and from Office 365.
+
+## Contact information
+
+hannes.palmquist@outlook.com
