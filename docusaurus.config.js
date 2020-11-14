4a053acf2f6bdda22aabd29efb113384e0922dff
Hannes Palmquist <33247576+hanpq@users.noreply.github.com>
2020-11-14 21:16:51 +0100

    Create docusaurus.config.js

---
+++ docusaurus.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  url: 'https://optimistic-benz-9cc6ad.netlify.app', // Url to your site with no trailing slash
+  baseUrl: '/', // Base directory of your site relative to your repo
+  // ...
+};
