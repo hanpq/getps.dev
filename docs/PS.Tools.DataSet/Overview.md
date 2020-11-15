c722edd37e7675fd81cdac9fad785f57567dc89f
Hannes Palmquist <33247576+hanpq@users.noreply.github.com>
2020-11-15 10:45:12 +0100

    Add PS.Tools.DataSet documentation

--- docs/PS.Tools.DataSet/Overview.md
+++
@@ -1,6 +0,0 @@
-﻿---
-id: dataset_overview
-title: PS.Tools.DataSet Overview
----
-
-This is a overview description of the module PS.Tools.DataSet
--- sidebars.js
+++ sidebars.js
@@ -2,6 +2,7 @@ module.exports = {
     someSidebar: {
         Docusaurus: ['doc1', 'doc2', 'doc3'],
         Features: ['mdx'],
-        Snippets: ['casting_datetime']
+        Snippets: ['casting_datetime'],
+        "PS.Tools.DataSet": "PS.Tools.DataSet"
     },
 };
