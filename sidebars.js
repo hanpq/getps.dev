ac1710d719d4b0b81d5e0373139c986de4894e35
Hannes Palmquist <33247576+hanpq@users.noreply.github.com>
2020-11-15 10:58:33 +0100

    Add PS.Tools.DataSet documentation

--- sidebars.js
+++ sidebars.js
@@ -3,14 +3,14 @@ module.exports = {
         Docusaurus: ['doc1', 'doc2', 'doc3'],
         Features: ['mdx'],
         Snippets: ['casting_datetime'],
-        "Command Reference": [
-            'commands/Add-DataSetRelation',
-            'commands/Add-DataSetTable',
-            'commands/Add-DataTableColumn',
-            'commands/Add-DataTableRow',
-            'commands/Get-DataTableRow',
-            'commands/New-DataSet',
-            'commands/New-DataTable'            
+        "PS.Tools.DataSet": [
+            'PS.Tools.DataSet/Add-DataSetRelation',
+            'PS.Tools.DataSet/Add-DataSetTable',
+            'PS.Tools.DataSet/Add-DataTableColumn',
+            'PS.Tools.DataSet/Add-DataTableRow',
+            'PS.Tools.DataSet/Get-DataTableRow',
+            'PS.Tools.DataSet/New-DataSet',
+            'PS.Tools.DataSet/New-DataTable'            
         ]
     },
 };
