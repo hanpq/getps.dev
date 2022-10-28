5f5e78a62e74e61bb4ec7f0e4c2724038698c879
Hannes Palmquist <hannes.palmquist@outlook.com>
2022-10-28 23:45:35 +0200

    Updated docs

--- modules/PSPortainer/commands/docusaurus.sidebar.js
+++ modules/PSPortainer/commands/docusaurus.sidebar.js
@@ -12,9 +12,14 @@ module.exports = [
     'PSPortainer/commands/Connect-Portainer',
     'PSPortainer/commands/Disconnect-Portainer',
     'PSPortainer/commands/Get-PContainer',
+    'PSPortainer/commands/Get-PContainerProcess',
+    'PSPortainer/commands/Get-PContainerStatistic',
     'PSPortainer/commands/Get-PEndpoint',
     'PSPortainer/commands/Get-PSession',
     'PSPortainer/commands/Get-PSettingsPublic',
     'PSPortainer/commands/Get-PStatus',
-    'PSPortainer/commands/Select-PEndpoint'
+    'PSPortainer/commands/Restart-PContainer',
+    'PSPortainer/commands/Select-PEndpoint',
+    'PSPortainer/commands/Start-PContainer',
+    'PSPortainer/commands/Stop-PContainer'
 ];
