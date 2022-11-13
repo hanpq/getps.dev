06a55478072abb897dd7dbb2201c99625fab6d99
Hannes Palmquist <hannes.palmquist@outlook.com>
2022-11-13 22:54:43 +0100

    .

--- sidebars_modules_dev.js
+++ sidebars_modules_dev.js
@@ -1,18 +1,10 @@
 ﻿// Start of file
-const pstoolsdaikin = require('./modules/pstools.daikin/sidebar.js');
 const pstoolsdataset = require('./modules/pstools.dataset/sidebar.js');
 const pstoolsqueue = require('./modules/pstools.queue/sidebar.js');
-const pstoolssortlibrary = require('./modules/pstools.sortlibrary/sidebar.js');
 module.exports = {
     modulessidebar: {
         Introduction: ['Inventory'],
         Modules: [
-            {
-                type: 'category',
-                label: 'pstools.daikin',
-                collapsed: true,
-                items: pstoolsdaikin
-            },
             {
                 type: 'category',
                 label: 'pstools.dataset',
@@ -34,4 +26,3 @@ module.exports = {
         ],
     },
 };
-
